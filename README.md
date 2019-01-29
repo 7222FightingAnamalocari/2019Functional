a088218e306088b04b725c7fe77e6db088581477
7222FightingAnamalocari <43886361+7222FightingAnamalocari@users.noreply.github.com>
2019-01-29 10:17:25 -0500

    Updating branch (#2)
    * REEE
    
    * Fixed?
    
    * smooth drive?
    
    * Readme improvement
    
    * Spicing up the Readme a bit more
    
    * Update README.md
    
    * can't speel
    
    * making stuff look nice
    
    * Better punctuation

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # 2019Functional
-Team 7222 FRC functional code 2019
-REEEEEEEEEEEEEEE
+Team 7222 FRC functional code for 2019
+
+This is the functioning code for FRC team 7222 in the 2019 Deep Space Competition
+The code is written in Java and uses the iterative robot setup. It has the ability to use 1 camera,
+4 drive motors, and 1 Joystick off the bat. Our code can be adapted to your using purposes if you
+can/want to.
+
+If you have no idea what is going on, either A, go to a psychologist or a doctor, or B, navigate to the code by following the path that starts on src. But if A, or B are true you probably wouldn't understand, or care
+to understand the code in the first place.
+
+# Disclaimer
+If you do set our code up wrong and it causes damage to your bot, we are not reliable
+for any damages. But we do send our apologies.
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,150 @@
+package frc.robot;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends IterativeRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  Joystick stick0 = new Joystick(0);
+
+  Spark m_leftFront = new Spark(0);
+  Spark m_leftBack = new Spark(1);
+  Spark m_rightFront = new Spark(2);
+  Spark m_rightBack = new Spark(3);
+
+  SpeedControllerGroup m_left = new SpeedControllerGroup(m_leftFront, m_leftBack);
+  SpeedControllerGroup m_right = new SpeedControllerGroup(m_rightFront, m_rightBack);
+
+  DoubleSolenoid cannonFeed = new DoubleSolenoid(1, 2);
+  
+  Compressor compressor = new Compressor(0);
+
+  boolean toggleOn = false;
+  boolean togglePressed = false;
+
+  boolean enabled = compressor.enabled();
+  boolean pressureSwitch = compressor.getPressureSwitchValue();
+  double current = compressor.getCompressorCurrent();
+
+  @Override
+  public void robotInit() {
+    m_chooser.addDefault("Default Auto", kDefaultAuto);
+    m_chooser.addObject("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    new Thread(() -> {
+      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+      camera.setResolution(640, 480);
+
+      CvSink cvSink = CameraServer.getInstance().getVideo();
+      CvSource outputStream = CameraServer.getInstance().putVideo("cam0", 640, 480);
+
+      Mat source = new Mat();
+      Mat output = new Mat();
+      while(!Thread.interrupted()) {
+        cvSink.grabFrame(source);
+        Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+        outputStream.putFrame(output);
+      }
+    }).start();
+    m_left.setInverted(true);
+  }
+
+ /*
+  * This function is called every robot packet, no matter the mode. Use
+  * this for items like diagnostics that you want ran during disabled,
+  * autonomous, teleoperated and test.
+  *
+  * <p>This runs after the mode specific periodic functions, but before
+  * LiveWindow and SmartDashboard integrated updating.
+  */
+  @Override
+  public void robotPeriodic() {
+  }
+
+ /*
+  * This autonomous (along with the chooser code above) shows how to select
+  * between different autonomous modes using the dashboard. The sendable
+  * chooser code works with the Java SmartDashboard. If you prefer the
+  * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+  * getString line to get the auto name from the text box below the Gyro
+  *
+  * <p>You can add additional auto modes by adding additional comparisons to
+  * the switch structure below with additional strings. If using the
+  * SendableChooser make sure to add them to the chooser code above as well.
+  */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // autoSelected = SmartDashboard.getString("Auto Selector",
+    // defaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    double stick0Y = stick0.getY();
+    double stick0X = stick0.getX();
+    
+    if((stick0Y >= .2) || (stick0Y <= -.2)) {
+      m_left.set(stick0Y/1.2);
+      m_right.set(stick0Y/1.2);
+    } else {
+      if((stick0X >= .3) || (stick0X <= -.3)) {
+        m_left.set(-stick0X/2);
+        m_right.set(stick0X/2);
+      }
+      m_left.set(0);
+      m_right.set(0);
+    }
+    compressor.setClosedLoopControl(true);
+    if(toggleOn) {
+      cannonFeed.set(DoubleSolenoid.Value.kForward);
+    } else {
+      cannonFeed.set(DoubleSolenoid.Value.kForward);
+    }
+    {
+      if(stick0.getRawButton(1)) {
+        if(!togglePressed) {
+          toggleOn = !toggleOn;
+          togglePressed = true;
+        }
+      } else {
+        togglePressed = false;
+      }
+    }
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+}
