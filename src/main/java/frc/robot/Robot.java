f8453e2da4b8ecbf129c08cbe6e38a535ff82661
7222FightingAnamalocari <43886361+7222FightingAnamalocari@users.noreply.github.com>
2019-02-25 10:45:50 -0500

    Updating stuff
    for Tim

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,8 +3,11 @@ package frc.robot;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.CameraServer;
+//import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+//import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.cscore.CvSink;
@@ -16,40 +19,57 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Robot extends IterativeRobot {
+public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
   Joystick stick0 = new Joystick(0);
+  Joystick stick1 = new Joystick(1);
 
   Spark m_leftFront = new Spark(0);
   Spark m_leftBack = new Spark(1);
   Spark m_rightFront = new Spark(2);
   Spark m_rightBack = new Spark(3);
 
+  Spark elevator0 = new Spark(4);
+  Spark elevator1 = new Spark(5);
+
+  Spark intakeRight = new Spark(6);
+  Spark intakeLeft = new Spark(7);
+
+  Spark driveBottom = new Spark(8);
+
   SpeedControllerGroup m_left = new SpeedControllerGroup(m_leftFront, m_leftBack);
   SpeedControllerGroup m_right = new SpeedControllerGroup(m_rightFront, m_rightBack);
 
-  DoubleSolenoid cannonFeed = new DoubleSolenoid(1, 2);
+  DifferentialDrive r_drive = new DifferentialDrive(m_left, m_right);
+
+  DoubleSolenoid sol0 = new DoubleSolenoid(0, 1);
+  DoubleSolenoid sol1 = new DoubleSolenoid(2, 3);
+  DoubleSolenoid sol2 = new DoubleSolenoid(4,5);
+  DoubleSolenoid sol3 = new DoubleSolenoid(6,7);
   
   Compressor compressor = new Compressor(0);
 
   boolean toggleOn = false;
+  boolean frontToggle = false;
+  boolean backToggle = false;
+
   boolean togglePressed = false;
+  boolean frontPressed = false;
+  boolean backPressed = false;
 
   boolean enabled = compressor.enabled();
-  boolean pressureSwitch = compressor.getPressureSwitchValue();
-  double current = compressor.getCompressorCurrent();
 
   @Override
   public void robotInit() {
     m_chooser.addDefault("Default Auto", kDefaultAuto);
     m_chooser.addObject("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    new Thread(() -> {
-      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+    new Thread (() -> {
+      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(0);
       camera.setResolution(640, 480);
 
       CvSink cvSink = CameraServer.getInstance().getVideo();
@@ -64,20 +84,19 @@ public class Robot extends IterativeRobot {
       }
     }).start();
     m_left.setInverted(true);
+    intakeLeft.setInverted(true);
   }
-
- /*
-  * This function is called every robot packet, no matter the mode. Use
-  * this for items like diagnostics that you want ran during disabled,
-  * autonomous, teleoperated and test.
-  *
-  * <p>This runs after the mode specific periodic functions, but before
-  * LiveWindow and SmartDashboard integrated updating.
-  */
   @Override
   public void robotPeriodic() {
+    //This function is called every robot packet, no matter the mode.
   }
-
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // autoSelected = SmartDashboard.getString("Auto Selector",
+    // defaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
  /*
   * This autonomous (along with the chooser code above) shows how to select
   * between different autonomous modes using the dashboard. The sendable
@@ -90,14 +109,6 @@ public class Robot extends IterativeRobot {
   * SendableChooser make sure to add them to the chooser code above as well.
   */
   @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // autoSelected = SmartDashboard.getString("Auto Selector",
-    // defaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
-
-  @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
       case kCustomAuto:
@@ -109,41 +120,151 @@ public class Robot extends IterativeRobot {
         break;
     }
   }
-
   @Override
   public void teleopPeriodic() {
-    double stick0Y = stick0.getY();
-    double stick0X = stick0.getX();
-    
-    if((stick0Y >= .2) || (stick0Y <= -.2)) {
-      m_left.set(stick0Y/1.2);
-      m_right.set(stick0Y/1.2);
-    } else {
-      if((stick0X >= .3) || (stick0X <= -.3)) {
-        m_left.set(-stick0X/2);
-        m_right.set(stick0X/2);
+    /*
+    new Thread(() -> {
+      double stick0Y = stick0.getY();
+      double stick0X = stick0.getX();
+      if((stick0Y >= .2) || (stick0Y <= -.2)) {
+        m_left.set(stick0Y/1.2);
+        m_right.set(stick0Y/1.2);
+      } else {
+        if(((stick0X == 0) && (stick0Y ==0)) ) {
+          m_left.set(0);
+          m_right.set(0);
+        }
       }
-      m_left.set(0);
-      m_right.set(0);
-    }
+    }).start();
+
+    new Thread (() -> {
+      double stick0X = stick0.getX();
+      if((stick0X >= .3) || (stick0X <= -.3)) {
+        m_left.set(stick0X/1.2);
+        m_right.set(-stick0X/1.2);
+      }
+    }).start();
+*/
     compressor.setClosedLoopControl(true);
-    if(toggleOn) {
-      cannonFeed.set(DoubleSolenoid.Value.kForward);
-    } else {
-      cannonFeed.set(DoubleSolenoid.Value.kForward);
+    new Thread(() -> {
+      r_drive.arcadeDrive(stick0.getY(), stick0.getX());
+    }).start();
+    new Thread (() -> { //bot lift codeEA
+      {
+      if(frontToggle) { //this is the front lift
+        sol0.set(DoubleSolenoid.Value.kForward);
+      } else {
+        sol0.set(DoubleSolenoid.Value.kReverse);
+      }
+    
+      if(stick0.getRawButton(11) == true) { //more front lift
+        if(frontPressed) {
+          frontToggle = !frontToggle;
+          frontPressed = true;
+        } else {
+        frontPressed = false;
+        }
+      }
     }
+  }).start();
+  new Thread(() -> {
     {
-      if(stick0.getRawButton(1)) {
-        if(!togglePressed) {
-          toggleOn = !toggleOn;
-          togglePressed = true;
+      if(backToggle) { //back lift
+        sol1.set(DoubleSolenoid.Value.kForward);
+      } else {
+        sol1.set(DoubleSolenoid.Value.kReverse);
+      }
+
+      if(stick0.getRawButton(12) == true) { //more back lift
+        if(backPressed) {
+          backToggle = !backToggle;
+          backPressed = true;
         }
       } else {
-        togglePressed = false;
+        backPressed = false;
+       }
       }
-    }
-  }
+  }).start();
 
+  new Thread(() -> { //elevator code
+    if((stick1.getRawAxis(1) >= .2) || (stick1.getRawAxis(1) <= -.2)) { //first level elevator
+      elevator0.set(-stick1.getRawAxis(1));
+    } else {
+      elevator0.set(0);
+    }
+  }).start();
+  new Thread(() -> {
+    if((stick1.getRawAxis(3) >= .2) || (stick1.getRawAxis(3) <= -.2)) { //second elevator
+      elevator1.set(stick1.getRawAxis(3));
+    } else {
+      elevator1.set(0);
+    }
+  }).start();
+
+  new Thread(() -> { //dover intake code
+      if(stick1.getPOV() == 1) {
+        intakeLeft.set(1);
+        intakeRight.set(1);
+      } else {
+        if(stick1.getPOV() == 3) {
+          intakeLeft.set(-1);
+          intakeRight.set(-1);
+        } else {
+          intakeLeft.set(0);
+          intakeRight.set(0);
+        }
+      }
+    }).start();
+
+    new Thread(() -> {
+      if(stick0.getPOV() == 1) {
+        driveBottom.set(.3);
+      } else {
+        if(stick0.getPOV() == 3) {
+          driveBottom.set(-.3);
+        } else {
+          driveBottom.set(0);
+        }
+      }
+    }).start();
+/*
+    new Thread(() -> {
+      {
+        if(toggleOn) {
+          sol2.set(DoubleSolenoid.Value.kForward);
+        } else {
+          sol2.set(DoubleSolenoid.Value.kReverse);
+        }
+        if(stick0.getRawButton(7) == true) { //more back lift
+          if(!this.togglePressed) {
+            this.toggleOn = !toggleOn;
+            this.togglePressed = true;
+          }
+        } else {
+          this.togglePressed = false;
+        }
+      }
+    }).start();
+
+    new Thread(() -> {
+      {
+        if(this.toggleOn) {
+          sol2.set(DoubleSolenoid.Value.kForward);
+        } else {
+          sol2.set(DoubleSolenoid.Value.kReverse);
+        }
+        if(stick0.getRawButton(8) == true) { //more back lift
+          if(!this.togglePressed) {
+            this.toggleOn = !toggleOn;
+            this.togglePressed = true;
+          }
+        } else {
+          this.togglePressed = false;
+        }
+      }
+    }).start();
+    */
+  }
   @Override
   public void testPeriodic() {
   }
